3349971f73a5c1037d66b1410cb66e04aa0c8b56
Adrian Schlichtmeier <Adrian.Schlichtmeier@student.uibk.ac.at>
2024-04-01 12:23:07 +0200

    first

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+value = 42
+answer = 'answer'
+print(answer, 'is', value)
+print(answer, 'is', value, sep='__')
+print(answer, 'is', value, sep='_', end='!')
+print(' <- this is not the beginning of the line', end='!\n')
+print('<- this is a new line')
+
+
+# Press the green button in the gutter to run the script.
+#if __name__ == '__main__':
+   # print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
